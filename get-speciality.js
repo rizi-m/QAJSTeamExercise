39d3dea4669f26c64e134624e59e995f4443c688
Mohammed Almas <deriftslayer@yahoo.co.uk>
2019-02-07 17:50:04 +0000

    data obtained is is being shown2

--- get-speciality.js
+++ get-speciality.js
@@ -27,33 +27,63 @@ function processFirst(member)
 {
     let baseButton = document.getElementById("baseButton");
     baseButton.innerHTML = member["firstName"] + " " + member["lastName"];;
-     
+    let basePanel = document.getElementById("basePanel");
+    for(key in member)
+    {
+
+    }
 }
 
 function createMember(member)
 {
     let baseButton = document.getElementById("baseButton");
-    let clone = baseButton.cloneNode(true);
-    //document.getElementById("buttonPlacement").appendChild(clone);
+    let basePanel = document.getElementById("basePanel");
+    let cloneButton = baseButton.cloneNode(true);
+    let clonePanel = basePanel.cloneNode(true);
 
-    //let baseButton = document.getElementById("baseButton");
-    //let basePanel = document.getElementById("basePanel");
+    cloneButton.innerHTML = member["firstName"] + " " + member["lastName"];
+
+    cloneButton.id = member["firstName"]+member["lastName"]+member["id"];
+    clonePanel.id = "Panel"+member["firstName"]+member["lastName"]+member["id"];
+
+    cloneButton.addEventListener("click",function() 
+    {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight)
+        {
+            panel.style.maxHeight = null;
+        } 
+        else 
+        {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        } 
+    });
+
+    document.getElementById("buttonPlacement").appendChild(cloneButton);
+    document.getElementById("buttonPlacement").appendChild(clonePanel);
     for(key in member)
     {
         if(key=="specialties")
         {
-            console.log("speciality array function call to make it")
+            //console.log("speciality array function call to make it")
         }
         else
         {
-            console.log(member[key]);
+           // console.log(member[key]);
             if(key == "firstName" || key == "lastName")
             {
-                 baseButton.innerHTML = member["firstName"] + " " + member["lastName"];
+                
+                let element = createElement('p',member[key],key);
+                document.getElementById(clonePanel.id).appendChild(element);
+                //baseButton.innerHTML = member["firstName"] + " " + member["lastName"];
             }
         }
     }
-    document.getElementById("buttonPlacement").appendChild(clone);
+   var acc = document.getElementsByClassName("accordion");
+    var i;
+
+
 }
 
 getSpeciality();
