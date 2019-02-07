a7fa53180460697e8b0c03fbcee8a37f6232b11e
Rizwan Mahmood <rizwanmahmood255@gmail.com>
2019-02-07 14:30:56 +0000

    Added skeleton for project

---
+++ http-requests.js
@@ -0,0 +1,15 @@
+const getRequest = function(url, onload) {
+    const request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.responseText = 'json';
+    request.onload = () => onload(request.response);
+    request.send();
+};
+
+const postRequest = function(url, data, onload) {
+    const request = new XMLHttpRequest();
+    request.open('POST', url, true);
+    request.setRequestHeader('Content-type','application/json;charset=utf-8');
+    request.onload = () => onlaod(request.responseText);
+    request.send(data);
+}
